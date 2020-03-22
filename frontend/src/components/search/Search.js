b5eaeee0d0a6553c3b207c166cc7388fff6d0f7a
Bingling Dong <bingling.dong@stonybrook.edu>
2020-03-22 12:08:35 -0400

    added basic functions for home and search

--- frontend/src/components/search/Search.js
+++ frontend/src/components/search/Search.js
@@ -1,15 +1,47 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useLocation, Link, useHistory } from "react-router-dom";
 
 
 export default function Search(props) {
+    //DO we actually need this variable?
     const [SearchResult, setSearchResult] = useState(null);
+    const location = useLocation();
+    const history = useHistory();
+
     useEffect(() => {
         async function fetchData() {
-            console.log("fetching");
+            // const res = await axios.get(location.pathname);
+            // if(!res.data.error){
+            //     console.log(res.data.q);
+            // }
         };
         fetchData();
     });
+
+    async function handleSearch(event){
+        event.preventDefault();
+        history.push('/search/' + event.target.searchQuery.value);
+        //refresh. or whatever
+    }
+
     return (
-        <div>Search Result PAGE</div>
+        <div>Search Result PAGE searching 
+            <div>
+                <form onSubmit={handleSearch}>
+                Top Search Bar<input name="searchQuery"></input>
+                <button type="submit">Search</button>
+                </form>
+                
+            </div>
+            <div>
+                Left Filter
+            </div>
+            <div>
+                Right Search Result
+            </div>
+
+
+        </div>
     );
 }
