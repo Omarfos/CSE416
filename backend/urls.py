5babed707c0a80e67544028e290e6a3c6fd61930
Omar Amin <omaramin4@gmail.com>
2020-03-25 15:36:12 -0400

    Added search functionality

--- backend/urls.py
+++ backend/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('student/<slug:userid>/', views.student_profile, name='Student Profile'),
     path('login', views.login_internal, name='Login'),
-    path('register', views.register, name='Register'),
-    path('college/<str:name>', views.college, name='College Page')
+    path('search', views.search, name='Search'),
+    path('college/<str:name>', views.college, name='College')
 ]
 
--- backend/views.py
+++ backend/views.py
@@ -15,7 +15,6 @@ def index(request):
 
 
 def student_profile(request, userid):
-    print(userid)
     s = get_object_or_404(Student,userid=userid)
     r = model_to_dict(s)
     return JsonResponse(r)
@@ -37,7 +36,6 @@ def login_internal(request):
     return HttpResponse("SUCCESS: User logged in")
 
 def register(request):
-
 
     try:
         d = json.loads(request.body)
@@ -62,5 +60,37 @@ def college(request, name):
     college = get_object_or_404(College, name=name)
     r = model_to_dict(college)
     return JsonResponse(r) 
+
+
+def search(request):
+    d = request.GET
+    q = College.objects.all()
+    if 'ranking' in d:
+        low, high = tuple(d['ranking'].split(','))
+        q = q.filter(ranking__range=(low,high))
+    if 'name' in d:
+        q = q.filter(name__contains=d['name'])
+    if 'size' in d:
+        low, high = tuple(d['size'].split(','))
+        q = q.filter(size__range=(low,high))
+    if 'adm_rate' in d:
+        low, high = tuple(d['adm_rate'].split(','))
+        q = q.filter(adm_rate__range=(low,high))
+    if 'SAT_math' in d:
+        low, high = tuple(d['SAT_math'].split(','))
+        q = q.filter(SAT_math__range=(low,high))
+    if 'SAT_EBRW' in d:
+        low, high = tuple(d['SAT_EBRW'].split(','))
+        q = q.filter(SAT_EBRW__range=(low,high))
+    if 'ACT_composite' in d:
+        low, high = tuple(d['ACT_composite'].split(','))
+        q = q.filter(ACT_composite__range=(low,high))
+    if 'sort' in d:
+        q = q.order_by(d['sort'])
+       
+    #l = json.loads(query[0].majors)
+    #print(l[0])
+    r = serializers.serialize('json', q);
+    return JsonResponse(r, safe=False)
 
 
