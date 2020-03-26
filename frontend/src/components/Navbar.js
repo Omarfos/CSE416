3a15b3aff97039a698e0fc6336b50074f8eea9c6
Olesia Elfimova <leself40@gmail.com>
2020-03-26 13:10:56 -0400

    Search added to navbar on some pages

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,16 +5,21 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import {Link, useHistory}  from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
+import {Route,Switch, Redirect} from "react-router-dom";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   }, 
   navBar: {
-    backgroundColor: "#eef5ff",
+    backgroundColor: "#eef5ff"
   },
   title: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   button:{
     textDecoration:"none",
@@ -25,7 +30,19 @@ const useStyles = makeStyles(theme => ({
     '&:hover': {
       background: "hsla(240, 48%, 41%, 0.1)",
       color: "#5d6896"
-   },
+    }
+  },
+  search: {
+    minWidth:"300px",
+    width: "30vw",
+    height: "35px",
+    marginBottom: "5px",
+    marginTop: "5px",
+    marginRight: "300px"
+  },
+  input:{
+    width: "85%",
+    marginBottom: "20px"
   }
 }));
 
@@ -40,13 +57,33 @@ export default function Navbar (props){
     history.push('/');
   }
 
+  async function handleSearch(event){
+    event.preventDefault();
+    history.push('/search/' + event.target.searchQuery.value);
+  }
+
   return (
     <div className={classes.root}>
-      <AppBar position="static" className={classes.navBar}>
+      <AppBar position="fixed" className={classes.navBar}>
         <Toolbar>
           <Typography align="left" className={classes.title}>
               <Link className={classes.button} to="/" >C4ME</Link>
           </Typography>
+
+          {/* search on navigation bar for some pages */}
+          <Route exact path={["/search/:q", "/view_college/:id"]}>
+            <Paper component="form" className={classes.search} onSubmit={handleSearch}>
+            <InputBase
+                name="searchQuery"
+                className={classes.input}
+                placeholder="Search for College"
+            />
+            <IconButton type="submit" className={classes.iconButton} aria-label="search">
+                <SearchIcon />
+            </IconButton>
+            </Paper>
+          </Route>
+
           {props.user &&
               <React.Fragment>
                   <Link className={classes.button} to={"/profile/"+props.user}>Profile</Link>
