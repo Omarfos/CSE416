fe51b7eb03007013fe0178358a19178e17d5398b
Bingling Dong <bingling.dong@stonybrook.edu>
2020-04-12 00:03:25 -0400

    dropdown finally working. Fix bug

--- frontend/src/components/user/CollegeDropdown.js
+++ frontend/src/components/user/CollegeDropdown.js
@@ -4,29 +4,32 @@ import Autocomplete from '@material-ui/lab/Autocomplete';
 import collegeName from "./colleges.json";
 
 export default function MultipleSelect(props) {
-  const handleSelectName = (value) => {
+    
+    const handleSelectName = (value) => {
+        console.log(value);
       if(value!=null){
-        console.log(value.name);
         props.handleEditCollege(value.name);
       }
-  };
-  return (
-    <Autocomplete
-      disabled={props.disable} 
-      id="tags-outlined"
-      options={collegeName}
-      defaultValue={collegeName[0]}
-      getOptionLabel={(option) => option.name}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          variant="outlined"
-          label="College Name"
-          placeholder=""
-          
+    };
+
+    return (
+        <Autocomplete
+        disabled={props.disable} 
+        id="tags-outlined"
+        options={collegeName}
+        defaultValue={{name:props.application.college}}
+        getOptionLabel={(option) => option.name}
+        getOptionSelected={(option, value) => value.name === option.name}
+        renderInput={(params) => (
+            <TextField
+            {...params}
+            variant="outlined"
+            label="College Name"
+            placeholder=""
+            
+            />
+        )}
+        onChange={(e, v) => handleSelectName(v)}
         />
-      )}
-      onChange={(e, v) => handleSelectName(v)}
-    />
-  );
+    );
 }
