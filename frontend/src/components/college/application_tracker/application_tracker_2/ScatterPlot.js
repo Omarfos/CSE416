c48f417a2d096c68a7685e79b8f52164b85550a6
Olesia Elfimova <leself40@gmail.com>
2020-04-13 16:42:55 -0400

    Weighted Avergage of Exams Percentiles for Scatter Plot works

--- frontend/src/components/college/application_tracker/application_tracker_2/ScatterPlot.js
+++ frontend/src/components/college/application_tracker/application_tracker_2/ScatterPlot.js
@@ -2,11 +2,41 @@ import React, { useState } from 'react'
 import Plot from "react-plotly.js";
 import MenuItem from "@material-ui/core/MenuItem";
 import Select from "@material-ui/core/Select";
-import { SettingsEthernet } from '@material-ui/icons';
 
 function ScatterPlot(props) {
     const { students } = props
     const [ test, setTest ] = useState("ACT_composite")
+
+    function computeWA_SAT() {
+        students.map(student => {
+            let result = 0;
+            let weights = 0;
+            Object.keys(student).forEach(y => {
+                // console.log('key: ' + y)
+                if (y.substring(0,4)=="SAT_" && y != "SAT_math" && y!= "SAT_EBRW" && student[ y ] != null) {
+                  result += 0.05 * (student[y] / 800);
+                  weights += 0.05;
+                }
+            });
+            if(student.SAT != null && student.ACT_composite != null){
+                result += (1-weights)/2 * (student.SAT / 1600 + student.ACT_composite / 36)
+                weights = 1;
+            }else if(student.SAT != null){
+                result += (1-weights) * student.SAT / 1600;
+                weights = 1;
+            }else if(student.ACT_composite != null){
+                result += (1-weights) * student.ACT_composite / 36;
+                weights = 1;
+            }
+            result = Math.round(result/weights * 100)
+            // console.log("WAWAWA")
+            // console.log(result)
+            student.WA_SAT = result;
+        });
+    };
+
+    computeWA_SAT();
+
     return (
         <>
             <Plot
@@ -45,13 +75,6 @@ function ScatterPlot(props) {
                         name: "Other"
                     }
 
-                    // {
-                    //   x: [1, 2, 3, 4],
-                    //   y: [2, 6, 3],
-                    //   type: "scatter",
-                    //   mode: "markers",
-                    //   marker: { color: "red" }
-                    // }
                 ] }
                 layout={ { width: 1000, height: 800, title: "A Fancy Plot" } }
             />
@@ -62,7 +85,7 @@ function ScatterPlot(props) {
                 <MenuItem value="ACT_math">ACT math</MenuItem>
                 <MenuItem value="ACT_reading">ACT reading</MenuItem>
                 <MenuItem value="ACT_science">ACT science</MenuItem>
-
+                <MenuItem value="WA_SAT">Weighted Average of Percentile Scores for Standardized Tests</MenuItem>
             </Select>
         </>
     )
