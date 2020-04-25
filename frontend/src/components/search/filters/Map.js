e2e0d5e164523af42e0b5d91d232aa7c42ec4e87
Olesia Elfimova <leself40@gmail.com>
2020-04-25 11:52:18 -0400

    States Map on Hover Display state name

--- frontend/src/components/search/filters/Map.js
+++ frontend/src/components/search/filters/Map.js
@@ -1,12 +1,28 @@
 // Normally these would be "import" statements, but this is a simple demo and
 // is needed to render in the browser.
 import React, { useState, useEffect } from "react";
+import { withStyles } from '@material-ui/core/styles';
+import { green } from '@material-ui/core/colors';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Tooltip from '@material-ui/core/Tooltip';
 
 // const { useEffect, useState } = React;
 const handleClick = (values) => {
     // let states = values.map((v) => v.code);
     // props.navigate(props.id, states);
 };
+
+const GreenCheckbox = withStyles({
+  root: {
+    color: green[400],
+    '&$checked': {
+      color: green[600],
+    },
+  },
+  checked: {},
+})((props) => <Checkbox color="default" {...props} />);
 
 
 
@@ -27,7 +43,9 @@ const USMap = (props) => {
   return (
     <svg viewBox="0 0 960 600">
       {statesData.map((stateData, index) =>
+      <Tooltip disableFocusListener title={stateData.name}>
         <path
+          id={"MyPath"+stateData.id}
           className="someCSSClass"
           style={{cursor: "pointer", fill: stateRegion(stateData.id)}}
           key={index}
@@ -49,7 +67,15 @@ const USMap = (props) => {
         //   }}
         >
         </path>
+        </Tooltip>
       )}
+      {/* {statesData.map((stateData, index) =>
+        <text style={{ fontSize: '30px'}}>
+          <textPath href={"#MyPath"+stateData.id}>
+              {stateData.id}
+          </textPath>
+      </text>
+      )} */}
     </svg>
   )
 }
@@ -59,6 +85,17 @@ const USMap = (props) => {
 export default function Map(props) {
   // The statesData is null by default until we set it.
   const [statesData, setStatesData] = useState(null);
+
+  const [state, setState] = React.useState({
+    checkedA: true,
+    checkedB: true,
+    checkedF: true,
+    checkedG: true,
+  });
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
 
   // This should only run once due to the [] arg for the dependencies.
   useEffect(() => {
@@ -78,7 +115,13 @@ export default function Map(props) {
   }
 
   return (
-    <USMap statesData={statesData} id={props.id} navigate={props.navigate}/>
+    <div>
+      <USMap statesData={statesData} id={props.id} navigate={props.navigate}/>
+      <FormControlLabel
+        control={<GreenCheckbox checked={state.checkedG} onChange={handleChange} name="checkedG"/>}
+        label="South"
+      />
+    </div>
   );
 }
 
