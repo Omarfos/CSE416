57dc8a601059a382932b374c226c4d0a3eaf57da
Omar Amin <omaramin4@gmail.com>
2020-04-29 14:45:43 -0400

    setting cors

--- c4me/settings.py
+++ c4me/settings.py
@@ -52,7 +52,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+]
+
 
 ROOT_URLCONF = 'c4me.urls'
 
