cde35d6e07ebab642d50dc207e61575449280a85
Bingling Dong <bingling.dong@stonybrook.edu>
2020-05-01 22:40:23 -0400

    dropdown ui

--- frontend/src/components/user/SelectionDropdown.js
+++ frontend/src/components/user/SelectionDropdown.js
@@ -5,7 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     textfield:{
-        marginTop: "20px",
+        marginTop: "15px",
         marginBottom: "20px",
     },
 }))
